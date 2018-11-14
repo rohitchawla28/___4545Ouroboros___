a61b4de169f938c3def01065b262291409fd84fa
gschmully <41164845+gschmully@users.noreply.github.com>
2018-11-13 21:56:49 -0600

    Test
    - In depth analysis of how Rohit's brain functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/RohitBrainTest.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.Testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class RohitBrainTest extends OpMode {
+
+    public void init() {
+        telemetry.addLine("Neural Network Created");
+        telemetry.addLine("Network Mirrors Rohit's Brain Activity");
+        telemetry.update();
+    }
+
+    public void loop() {
+        telemetry.addLine("Cannot Understand GitHub");
+        telemetry.update();
+    }
+}
