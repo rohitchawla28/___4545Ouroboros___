e9fd49534573f586a9bff8ccd91e0b97077ce9a4
anishpandya <42559761+anishpandya@users.noreply.github.com>
2018-12-11 17:29:13 -0600

    Implemented openCV module
    - problem with System.loadLibrary()?
    - Error: Cannot open RobotControllerApp
    - continued grip filter work (newer grip filter not in this edition.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OpenCVTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Testing/OpenCVTest.java
@@ -4,34 +4,39 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.ChickHicks.Vision.OpenCVDetection;
+import org.firstinspires.ftc.teamcode.ChickHicks.Vision.Vuforia;
+
 //import ChickHicks.Vision;
 //import ChickHicks.Vuforia;
 
-@Disabled
+//@Disabled
 @Autonomous
         (name = "OpenCVTest", group = "Auto")
 
 public class OpenCVTest extends LinearOpMode {
 
-//    private Vision vision;
-//    private Vuforia vuforia;
+  private OpenCVDetection vision;
+  private Vuforia vuforia;
 
     @Override
     public void runOpMode() throws InterruptedException {
 
-//        vuforia = new Vuforia(this);
-//        vision = new Vision(this);
+
+        vision = new OpenCVDetection(this);
+
+        vuforia = new Vuforia(this);
 
         telemetry.addLine("Initialized");
         telemetry.update();
 
-        waitForStart();
+
 
 
         while(opModeIsActive())
         {
-//            vision.process(vuforia.convertToMat());
-//            telemetry.addData("Cube Position ", Vision.cubePositionAlt);
+            vision.process(vuforia.convertToMat());
+            telemetry.addData("Cube Position ", OpenCVDetection.cubePositionAlt);
             telemetry.update();
         }
 
