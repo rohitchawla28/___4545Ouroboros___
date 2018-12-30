d420b4feb7d34d89649270c5bf059e9c252c649a
rohitchawla28 <31925602+rohitchawla28@users.noreply.github.com>
2018-12-30 03:11:31 -0600

    Auto
    - made starting framework with vision code and detatching code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FinalAutos/Crater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/FinalAutos/Crater.java
@@ -14,33 +14,33 @@ import org.firstinspires.ftc.teamcode.ChickHicks.Vision.Vuforia;
 
 public class Crater extends LinearOpMode{
     Drivetrain drivetrain;
-//    Lift lift;
-    //OpenCVDetection vision;
-   // Vuforia vuforia;
+    Lift lift;
+    // Vuforia vuforia;
+    // OpenCVDetection vision;
+
 
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         drivetrain = new Drivetrain(this);
-       // vuforia = new Vuforia(this);
-       // vision = new OpenCVDetection(this, vuforia);
+        // vuforia = new Vuforia(this);
+        // vision = new OpenCVDetection(this, vuforia);
+         lift = new Lift(this);
 
         telemetry.addLine(" Init finished");
         telemetry.update();
 
-        //lift = new Lift(this);
-
         waitForStart();
 
-//        while (opModeIsActive())
-//        {
-//            vision.process(vuforia.convertToMat());
-//
-//            telemetry.addLine("The cube is in position " + vision.cubePositionAlt);
-//            telemetry.update();
-//
-//        }
+        // vision.process(vuforia.convertToMat());
+
+        // telemetry.addLine("The cube is in position " + vision.cubePositionAlt);
+        // telemetry.update();
+
+        // lift.detachEncoder(drivetrain);
+
+        // lift.extendSampling(drivetrain, vuforia);
 
         drivetrain.turnPI(90, true, .33/90, 0.013, 15);
     }
